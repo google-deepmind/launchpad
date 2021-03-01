4c85efc3535bf18b80f94301b14717da06359ec0
Fan Yang <fanyan@google.com>
2021-03-01 03:36:37 -0800

    Internal change.
    PiperOrigin-RevId: 360149094
    Change-Id: I55d72a745411c57ce3510d355861a9147583728c

--- launchpad/launch/launch.py
+++ launchpad/launch/launch.py
@@ -15,6 +15,7 @@
 
 """Launch program in different ways based on FLAGS.lp_launch_type."""
 
+import sys
 from typing import Any, Dict, Mapping, Optional, Sequence, Union, cast
 
 from absl import flags
