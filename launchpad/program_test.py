e283a96619efc866045682a60723e0e09905141e
LaunchPad Team <dm-launchpad-dev@google.com>
2021-04-30 02:43:31 -0700

    Add autospec to program tests.
    PiperOrigin-RevId: 371295867
    Change-Id: I4fe39b1d5db66e0a4779ae3b02804a60a87df565

--- launchpad/program_test.py
+++ launchpad/program_test.py
@@ -18,27 +18,29 @@
 from unittest import mock
 
 from absl.testing import absltest
+
 from launchpad import program as lp_program
+from launchpad.nodes import base
 
 
 class ProgramTest(absltest.TestCase):
 
   def test_add_node(self):
     program = lp_program.Program('foo')
-    node_foo = mock.Mock()
+    node_foo = mock.Mock(autospec=base.Node)
     program.add_node(node_foo, 'foo')
-    node_bar = mock.Mock()
+    node_bar = mock.Mock(autospec=base.Node)
     program.add_node(node_bar, 'bar')
     self.assertEqual(program.groups['foo'], [node_foo])
     self.assertEqual(program.groups['bar'], [node_bar])
 
   def test_add_nodes_to_group(self):
     program = lp_program.Program('foo')
-    nodes_foo = [mock.Mock(), mock.Mock()]
+    nodes_foo = [mock.Mock(autospec=base.Node), mock.Mock(autospec=base.Node)]
     with program.group('foo'):
       for node in nodes_foo:
         program.add_node(node)
-    nodes_bar = [mock.Mock(), mock.Mock()]
+    nodes_bar = [mock.Mock(autospec=base.Node), mock.Mock(autospec=base.Node)]
     with program.group('bar'):
       for node in nodes_bar:
         program.add_node(node)
@@ -48,41 +50,41 @@ class ProgramTest(absltest.TestCase):
   def test_add_node_without_label(self):
     program = lp_program.Program('foo')
     with self.assertRaisesRegexp(ValueError, 'Label should not be empty.'):  
-      program.add_node(mock.Mock())
+      program.add_node(mock.Mock(autospec=base.Node))
 
   def test_add_node_with_empty_label(self):
     program = lp_program.Program('foo')
     with self.assertRaisesRegexp(ValueError, 'Label should not be empty.'):  
-      program.add_node(mock.Mock(), label='')
+      program.add_node(mock.Mock(autospec=base.Node), label='')
 
   def test_add_group_with_empty_label(self):
     program = lp_program.Program('foo')
     with self.assertRaisesRegexp(ValueError, 'Label should not be empty.'):  
       with program.group(''):
-        program.add_node(mock.Mock())
+        program.add_node(mock.Mock(autospec=base.Node))
 
   def test_use_label_in_node_and_group(self):
     program = lp_program.Program('foo')
     with self.assertRaisesRegexp(  
         ValueError, 'label does not match the current group'):
       with program.group('foo'):
-        program.add_node(mock.Mock(), label='bar')
+        program.add_node(mock.Mock(autospec=base.Node), label='bar')
 
     # If the label matches the current group, then it's ok.
     with program.group('foo'):
-      program.add_node(mock.Mock(), label='foo')
+      program.add_node(mock.Mock(autospec=base.Node), label='foo')
 
   def test_nested_groups(self):
     program = lp_program.Program('foo')
     with self.assertRaisesRegexp(ValueError, r'group\(\) cannot be nested.'):  
       with program.group('foo'):
         with program.group('bar'):
-          program.add_node(mock.Mock())
+          program.add_node(mock.Mock(autospec=base.Node))
 
   def test_duplicated_label(self):
     program = lp_program.Program('foo')
-    node1 = mock.Mock()
-    node2 = mock.Mock()
+    node1 = mock.Mock(autospec=base.Node)
+    node2 = mock.Mock(autospec=base.Node)
     program.add_node(node1, 'foo')
     program.add_node(node2, 'foo')
     with program.group('bar'):
@@ -95,16 +97,16 @@ class ProgramTest(absltest.TestCase):
   def test_get_all_nodes(self):
     nodes = []
     program = lp_program.Program('test')
-    node_foo = mock.Mock()
+    node_foo = mock.Mock(autospec=base.Node)
     nodes.append(node_foo)
     program.add_node(node_foo, 'foo')
 
-    node_bar = mock.Mock()
+    node_bar = mock.Mock(autospec=base.Node)
     nodes.append(node_bar)
     program.add_node(node_bar, 'bar')
 
     with program.group('baz'):
-      node_baz = mock.Mock()
+      node_baz = mock.Mock(autospec=base.Node)
       nodes.append(node_baz)
       program.add_node(node_baz)
     self.assertCountEqual(program.get_all_nodes(), nodes)
