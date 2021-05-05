0494fc3b51a7a02d804e8511a2c9a3657509c3cf
Piotr Stanczyk <stanczyk@google.com>
2021-05-05 07:58:29 -0700

    Make sure Launchpad doesn't import many modules for simple use cases.
    PiperOrigin-RevId: 372126110
    Change-Id: I6f7beb941bfed95a7b9574f453e6f4905b68cf1e

--- launchpad/examples/consumer_producers/launch_test.py
+++ launchpad/examples/consumer_producers/launch_test.py
@@ -15,6 +15,8 @@
 
 """Launchpad integration test for the consumer_producers example."""
 
+import sys
+
 from absl.testing import absltest
 import launchpad as lp
 from launchpad.examples.consumer_producers import launch
@@ -44,6 +46,8 @@ class LaunchTest(absltest.TestCase):
     # able to take 10 steps.
     for _ in range(10):
       consumer.step()
+    # Make sure executing lp.launch doesn't import excesive number of modules.
+    self.assertLess(len(sys.modules), 2500)
 
 
 if __name__ == '__main__':
