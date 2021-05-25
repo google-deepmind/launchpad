ebb198bfb458da813c9fa4f90b701dda9677a402
Fan Yang <fanyan@google.com>
2021-05-25 10:52:11 -0700

    Internal change to local launch
    PiperOrigin-RevId: 375743812
    Change-Id: I228500099c879674e0cf100623055a582f888ec2

---
+++ launchpad/launch/test_multi_processing/launch.py
@@ -0,0 +1,174 @@
+# Lint as: python3
+# Copyright 2020 DeepMind Technologies Limited. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Launches a Launchpad program as a test, using multiple processes.
+
+Usage:
+
+    class LaunchTest(absltest.TestCase):
+
+      def test_run_program(self):
+        program = ...  # Create a program
+
+        # Configure process resources
+        local_resources = dict(
+            foo=lp.PythonProcess(
+                interpreter=...
+            )
+        )
+
+        lp.launch(program, launch_type='test_mp',
+                  local_resources=local_resources, test_case=self)
+"""
+
+from concurrent import futures
+import os
+import signal
+import subprocess
+from typing import Any, Mapping, Optional, Sequence, Text
+
+from absl import logging
+from absl.testing import absltest
+from launchpad import context
+from launchpad import program as lp_program
+import psutil
+
+
+class _Processes:
+  """Encapsulates the running processes of a launched Program."""
+
+  def __init__(self):
+    """Initializes a _Processes."""
+    self.process_dict: Mapping[Text, Sequence[subprocess.Popen]] = {}
+    self._stop_requested = False
+    signal.signal(signal.SIGUSR1,
+                  lambda signum, unused_frame: self._stop())
+
+  def set_processes(self,
+                    process_dict: Mapping[Text, Sequence[subprocess.Popen]]):
+    """Set collection of processes to handle.
+
+    Args:
+      process_dict: Mapping from node group label to list of running processes
+        for that group.
+    """
+    self.process_dict = process_dict
+    if self._stop_requested:
+      self.kill_processes()
+
+  def _stop(self):
+    self._stop_requested = True
+    self.kill_processes()
+
+  def kill_processes(self):
+    for processes in self.process_dict.values():
+      for process in processes:
+        process.kill()
+
+  def wait(self, labels_to_wait_for: Optional[Sequence[Text]] = None):
+    """Wait for processes to finish.
+
+    Args:
+      labels_to_wait_for: If supplied, only wait for these groups' processes to
+        finish (but still raise an exception if any process from any group
+        fails).
+
+    Raises:
+      RuntimeError: if any process raises an exception.
+    """
+    processes_to_wait_for = set()
+    for label in (labels_to_wait_for
+                  if labels_to_wait_for is not None else self.process_dict):
+      processes_to_wait_for.update(self.process_dict[label])
+
+    all_processes = set()
+    for processes in self.process_dict.values():
+      all_processes.update(processes)
+    executor = futures.ThreadPoolExecutor(len(all_processes))
+
+    def waiter(p):
+      return lambda: (p, p.wait())
+
+    process_futures = [executor.submit(waiter(p)) for p in all_processes]
+    while processes_to_wait_for:
+      done, process_futures = futures.wait(
+          process_futures, return_when=futures.FIRST_COMPLETED)
+      for future in done:
+        unused_p, returncode = future.result()
+        if returncode != 0 and not self._stop_requested:
+          raise RuntimeError('One of the processes has failed!')
+      processes_to_wait_for.difference_update(f.result()[0] for f in done)
+
+  def ensure_healthy(self):
+    for label, procs_per_label in self.process_dict.items():
+      for process in procs_per_label:
+        if not psutil.pid_exists(process.pid) and process.returncode:
+          logging.error('Process %d of %s has crashed.', process.pid, label)
+          healthy = False
+    if not healthy:
+      raise RuntimeError(
+          'The processes are not all healthy (see logs above for details)!')
+
+
+def launch(program: lp_program.Program,
+           test_case: absltest.TestCase,
+           local_resources: Mapping[str, Any] = None):
+  """Launches a program using multiple processes as a test."""
+  # Set up the launch context (launch type & launch config) for all nodes
+  local_resources = local_resources or {}
+  for label, nodes in program.groups.items():
+    launch_config = local_resources.get(label, None)
+    for node in nodes:
+      node._initialize_context(  
+          context.LaunchType.TEST_MULTI_PROCESSING,
+          launch_config=launch_config)
+
+  # Notify the input handles
+  for label, nodes in program.groups.items():
+    for node in nodes:
+      for handle in node._input_handles:  
+        handle.connect(node, label)
+
+  # Bind addresses
+  for node in program.get_all_nodes():
+    node.bind_addresses()
+
+  label_to_commands = {}
+  process_handles = {}
+  for label, nodes in program.groups.items():
+    # to_executables() is a static method, so we can call it from any of the
+    # nodes in this group.
+    # Somehow pytype thinks to_executables() gets wrong arg count.
+    
+    # pytype: disable=wrong-arg-count
+    commands = nodes[0].to_executables(nodes, label, nodes[0]._launch_context)
+    
+    # pytype: disable=wrong-arg-count
+    if commands:
+      label_to_commands[label] = commands
+      # Not to create to actual processes, in case of failures in this loop.
+      process_handles[label] = []
+
+  processes = _Processes()
+  for label, commands in label_to_commands.items():
+    for command in commands:
+      env = {}
+      env.update(os.environ)
+      env.update(command.env_overrides)
+      process_handles[label].append(
+          subprocess.Popen(command.command_as_list, env=env))
+  processes.set_processes(process_handles)
+  test_case.addCleanup(processes.kill_processes)
+  return processes
---
+++ launchpad/launch/test_multi_processing/launch_test.py
@@ -0,0 +1,170 @@
+# Lint as: python3
+# Copyright 2020 DeepMind Technologies Limited. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for launchpad.launch.test_multi_processing.launch."""
+
+import time
+
+from absl.testing import absltest
+from launchpad import context
+from launchpad import program
+from launchpad.launch.test_multi_processing import launch
+from launchpad.nodes.python import local_multi_processing
+from launchpad.nodes.python import node as python
+from launchpad.program_stopper import program_stopper
+
+
+def _get_default_py_node_config():
+  return local_multi_processing.PythonProcess(
+      interpreter='third_party/py/launchpad/launch/'
+      'test_multi_processing/interpreter')
+
+
+def _noop():
+  pass
+
+
+def _fail():
+  raise RuntimeError('Some error.')
+
+
+def _block():
+  while True:
+    time.sleep(2)
+
+
+def _stop(stopper):
+  stopper()
+
+
+class LaunchTest(absltest.TestCase):
+
+  def test_wait_for_one(self):
+    p = program.Program('test')
+
+    p.add_node(python.PyNode(_noop), 'noop')
+    resources = dict(noop=_get_default_py_node_config())
+    processes = launch.launch(p, test_case=self, local_resources=resources)
+
+    p_dict = processes.process_dict
+    self.assertEqual(list(p_dict.keys()), ['noop'])
+    self.assertLen(p_dict['noop'], 1)
+    p_dict['noop'][0].wait()  # Wait until termination
+    self.assertEqual(p_dict['noop'][0].returncode, 0)
+
+  def test_wait_for_all(self):
+    p = program.Program('test')
+
+    p.add_node(python.PyNode(_noop), 'noop')
+    resources = dict(noop=_get_default_py_node_config())
+    processes = launch.launch(p, test_case=self, local_resources=resources)
+
+    processes.wait()
+
+  def test_wait_for_some_only_waits_for_specified_node_groups(self):
+    p = program.Program('test')
+
+    with p.group('main'):
+      p.add_node(python.PyNode(_noop))
+    with p.group('daemon'):
+      p.add_node(python.PyNode(_block))
+    resources = dict(
+        main=_get_default_py_node_config(),
+        daemon=_get_default_py_node_config())
+    processes = launch.launch(p, test_case=self, local_resources=resources)
+
+    processes.wait(['main'])
+
+  def test_wait_for_some_detects_exception_in_any_node_group(self):
+    p = program.Program('test')
+
+    with p.group('main'):
+      p.add_node(python.PyNode(_block))
+    with p.group('daemon'):
+      p.add_node(python.PyNode(_fail))
+    resources = dict(
+        main=_get_default_py_node_config(),
+        daemon=_get_default_py_node_config())
+    processes = launch.launch(p, test_case=self, local_resources=resources)
+
+    with self.assertRaisesRegexp(  
+        RuntimeError, 'One of the processes has failed'):
+      processes.wait(['main'])
+
+  def test_no_wait_after_failure(self):
+    p = program.Program('test')
+
+    p.add_node(python.PyNode(_fail), 'failer')
+    p.add_node(python.PyNode(_block), 'blocker')
+    resources = dict(
+        failer=_get_default_py_node_config(),
+        blocker=_get_default_py_node_config())
+    processes = launch.launch(p, test_case=self, local_resources=resources)
+
+    # One process crashes and the other blocks indefinitely. In this case wait()
+    # should return upon the first failure.
+    with self.assertRaisesRegexp(  
+        RuntimeError, 'One of the processes has failed'):
+      processes.wait()
+
+  def test_grouping(self):
+    # This verifies the process handles are grouped correctly
+    p = program.Program('test')
+
+    with p.group('foo'):
+      for _ in range(1):
+        p.add_node(python.PyNode(_noop))
+
+    with p.group('bar'):
+      for _ in range(2):
+        p.add_node(python.PyNode(_noop))
+
+    resources = dict(
+        foo=_get_default_py_node_config(), bar=_get_default_py_node_config())
+    processes = launch.launch(p, test_case=self, local_resources=resources)
+    p_dict = processes.process_dict
+    self.assertCountEqual(list(p_dict.keys()), ['foo', 'bar'])
+    self.assertLen(p_dict['foo'], 1)
+    self.assertLen(p_dict['bar'], 2)
+
+  def test_program_stopper(self):
+    # This verifies the program stopper works for test_multi_processing
+    p = program.Program('test')
+
+    with p.group('block'):
+      p.add_node(python.PyNode(_block))
+
+    with p.group('stop'):
+      p.add_node(python.PyNode(_stop, program_stopper.make_program_stopper(
+          context.LaunchType.TEST_MULTI_PROCESSING)))
+
+    resources = dict(
+        block=_get_default_py_node_config(), stop=_get_default_py_node_config())
+    processes = launch.launch(p, test_case=self, local_resources=resources)
+    processes.wait()
+
+  def test_cleanup(self):
+    # Test verifies that test cleanup works.
+    p = program.Program('test')
+
+    with p.group('block'):
+      p.add_node(python.PyNode(_block))
+
+    resources = dict(block=_get_default_py_node_config())
+    launch.launch(p, test_case=self, local_resources=resources)
+
+
+if __name__ == '__main__':
+  absltest.main()
