b7f95505e720bf01c5521099814c6bfb1d52f4d9
Piotr Stanczyk <stanczyk@google.com>
2021-12-23 02:55:57 -0800

    Do not remove handlers upon finalization. _stop_or_kill could have been destroyed already.
    PiperOrigin-RevId: 417974916
    Change-Id: I07eec1c5b2e63abed89ead5bcadaad1a15f17820

--- launchpad/launch/worker_manager.py
+++ launchpad/launch/worker_manager.py
@@ -366,10 +366,10 @@ class WorkerManager:
     self._stop_or_kill()
 
   def _disable_alarm(self):
-    _remove_signal_handler(signal.SIGALRM, self._stop_or_kill)
     if sys.is_finalizing():
       # See https://bugs.python.org/issue26133.
       return
+    _remove_signal_handler(signal.SIGALRM, self._stop_or_kill)
     signal.alarm(0)
 
   def stop_and_wait(self):
