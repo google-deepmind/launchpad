28cb53f9693e19d395468d6c3c3f1579a0772df6
Piotr Stanczyk <stanczyk@google.com>
2022-01-18 05:53:58 -0800

    Use new LazyImports all across Launchpad codebase.
    PiperOrigin-RevId: 422535856
    Change-Id: Ida17949ef8e2c4421b0664fb60c7901376fbcdb7

--- launchpad/lazy_loader.py
+++ launchpad/lazy_loader.py
@@ -16,7 +16,6 @@
 
 import importlib
 import sys
-import types
 import typing
 
 # Set to true in your local workspace to work around issues with lazy imports.
@@ -138,30 +137,3 @@ class _ModuleToLoad():
 
   def __getattr__(self, item):
     return _MemberToLoad(item, self)
-
-
-class LazyImport(types.ModuleType):
-  """Lazily import a module, mainly to avoid pulling in large dependencies."""
-
-  def __init__(self, local_name, parent_module_globals, name):
-    self._local_name = local_name
-    self._parent_module_globals = parent_module_globals
-    super(LazyImport, self).__init__(name)
-    if _DISABLE_LAZY_IMPORTS:
-      self._load()
-
-  def _load(self):
-    """Load the module and insert it into the parent's globals."""
-    # Import the target module and insert it into the parent's namespace
-    module = importlib.import_module(self.__name__)
-    self._parent_module_globals[self._local_name] = module
-    self.__dict__.update(module.__dict__)
-    return module
-
-  def __getattr__(self, item):
-    module = self._load()
-    return getattr(module, item)
-
-  def __dir__(self):
-    module = self._load()
-    return dir(module)
