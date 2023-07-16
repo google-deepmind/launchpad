53f4d8d9fe682d5e025bc4fc7809b65f88491b64
Fan Yang <fanyan@google.com>
2023-07-16 06:11:28 -0700

    Update reverb and tf versions
    PiperOrigin-RevId: 548486592
    Change-Id: I134fae49388e36280fbf51ef69936e1acf90d730

--- launchpad/pip_package/launchpad_version.py
+++ launchpad/pip_package/launchpad_version.py
@@ -17,8 +17,8 @@
 
 # We follow Semantic Versioning (https://semver.org/)
 _MAJOR_VERSION = '0'
-_MINOR_VERSION = '5'
-_PATCH_VERSION = '2'
+_MINOR_VERSION = '6'
+_PATCH_VERSION = '0'
 
 # When building releases, we can update this value on the release branch to
 # reflect the current release candidate ('rc0', 'rc1') or, finally, the official
@@ -37,8 +37,8 @@ __version__ = '.'.join([
 __dev_version__ = '{}.{}'.format(__version__, _DEV_SUFFIX)
 __rel_version__ = '{}{}'.format(__version__, _REL_SUFFIX)
 
-__tensorflow_version__ = 'tensorflow~=2.8.0'
-__reverb_version__ = 'dm-reverb==0.11.0'
+__tensorflow_version__ = 'tensorflow~=2.12.0'
+__reverb_version__ = 'dm-reverb==0.12.0'
 
 __nightly_tensorflow_version__ = 'tf-nightly'
 __nightly_reverb_version__ = 'dm-reverb-nightly'
